30861560c3ece4628d8ca65c1524af1d6e64a09f
Vap0r1ze <superdash993@gmail.com>
2023-12-06 01:51:45 -0500

    day 6

---
+++ src/06/index.ts
@@ -0,0 +1,41 @@
+import { random } from "../utils"
+
+interface Options {
+    RACES: number
+    TIME_RANGE: [number, number]
+}
+
+export const presets: Record<string, Options> = {
+    normal: {
+        RACES: 4,
+        TIME_RANGE: [40, 80],
+    },
+    large: {
+        RACES: 1000,
+        TIME_RANGE: [10000, 1600],
+    }
+}
+
+export function generate({ RACES, TIME_RANGE }: Options) {
+    const times = Array.from({ length: RACES }, (_, i) => {
+        return Math.floor(random(`time:${i}`) * (TIME_RANGE[1] - TIME_RANGE[0]) + TIME_RANGE[0])
+    })
+    const distances = times.map(time => {
+        const max = time**2/4 - 5
+        return Math.floor(random(`distance:${time}`) * max)
+    })
+
+    const bigTime = BigInt(times.join(""))
+    const bigDistance = BigInt(distances.join(""))
+    console.assert(bigTime**2n / 4n - 5n >= bigDistance, "Distance is too close to max distance")
+
+    let timesText = times.map(time => `${time}`)
+    let distancesText = distances.map(distance => `${distance}`)
+    timesText = timesText.map((time, i) => time.padStart(distancesText[i].length, " "))
+    distancesText = distancesText.map((distance, i) => distance.padEnd(timesText[i].length, " "))
+
+    return [
+        ["Time:    ", ...timesText].join("  "),
+        ["Distance:", ...distancesText].join("  "),
+    ].join("\n")
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { mkdirSync, writeFileSync } from "fs"
 import * as day5 from "./05"
+import * as day6 from "./06"
 
 Math.random = function () {
     throw new Error("Don't use Math.random()! Use random() from 'utils.ts' instead.")
@@ -7,11 +8,12 @@ Math.random = function () {
 
 const days = {
     "05": day5,
+    "06": day6,
 }
 for (const [number, day] of Object.entries(days)) {
     mkdirSync(`dist/${number}`, { recursive: true })
     for (const [preset, opts] of Object.entries(day.presets)) {
-        writeFileSync(`dist/${number}/input_${preset}.txt`, day.generate(opts))
+        writeFileSync(`dist/${number}/input_${preset}.txt`, day.generate(opts as never))
         console.log(`Generated dist/${number}/input_${preset}.txt`)
     }
 }
